10d50177072c50e721fc8d4cfe263e809c815d7f
omar faruk <faruk141pau@gmail.com>
2024-03-28 11:29:27 +0600

    Add project files.

---
+++ OnlineShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Models;
+
+namespace OnlineShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+        
+    }
+}
